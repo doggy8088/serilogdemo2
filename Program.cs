57864d91e9303253bbf5027754fceff18026b23e
Will 保哥 <doggy.huang@gmail.com>
2021-11-30 00:49:19 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+using Serilog;
+using Serilog.Events;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+try
+{
+    Log.Information("Starting web host");
+
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.WebHost.UseSerilog();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseSerilogRequestLogging(options =>
+    {
+        // 如果要自訂訊息的範本格式，可以修改這裡，但修改後並不會影響結構化記錄的屬性
+        options.MessageTemplate = "Handled {RequestPath}";
+
+        // 預設輸出的紀錄等級為 Information，你可以在此修改記錄等級
+        // options.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Debug;
+
+        // 你可以從 httpContext 取得 HttpContext 下所有可以取得的資訊！
+        options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+        {
+            diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+            diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+            diagnosticContext.Set("UserID", httpContext.User.Identity?.Name);
+        };
+    });
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
+    return 0;
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
